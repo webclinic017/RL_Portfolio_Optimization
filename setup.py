6f453a35a0a686921fbd1670017161f7fbef95ec
rg468 <rg468@cam.ac.uk>
2021-07-29 16:42:50 -0700

    readme, requirements and setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Project aims to use various Deep Reinforcement Learning methods to optimize a portfolio of stocks',
+    author='Sierra View RCM',
+    license='MIT',
+)
