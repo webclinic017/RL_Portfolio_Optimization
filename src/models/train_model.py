bc011918aaa00de32bbab2b83e5ca86657a04202
rg468 <rg468@cam.ac.uk>
2021-07-30 14:22:27 -0700

    "Implemented train_ppo for model training"

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,6 +1,24 @@
 import pandas as pd
 import numpy as np
 import time
+from src.models.env_train import StockEnv
+from stable_baselines.common.vec_env import DummyVecEnv
+from stable_baselines import PPO2
+
+
+def train_PPO(env_train, model_name, output_dir, timesteps=50000):
+    """PPO model"""
+    start = time.time()
+    model = PPO2('MlpPolicy', env_train, ent_coef=0.005, nminibatches=8)
+
+    print('PPO start training.')
+    model.learn(total_timesteps=timesteps)
+    end = time.time()
+
+    model.save("{}/{}".format(output_dir, model_name))
+    print('Training time (PPO): ', (end - start) / 60, ' minutes')
+
+    return model
 
 
 class Model(object):
@@ -10,7 +28,27 @@ class Model(object):
         self.training_dates = [(2, i) for i in range(1, 13)]
 
     def main(self):
-        pass
+        env_train = DummyVecEnv([lambda: StockEnv(self.training_dates)])
+
+        model = train_PPO(env_train, "PPO", "trained_models", timesteps=10000)
+
+        # trading_dates = [(1, i) for i in range(1, 13)]
+        #
+        # for date in trading_dates:
+        #     env_train = DummyVecEnv([lambda: StockEnv(self.training_dates)])
+        #     env_val = DummyVecEnv([lambda: StockEnv(date)])
+
+
+if __name__ == "__main__":
+
+    train_model = Model()
+
+    train_model.main()
+
+
+
+
+
 
 
 
