c6fc043f809073696ab671a8901c2c2c0e13ba1e
wslotkin <william.slotkin@gmail.com>
2021-12-25 12:06:12 -0500

    Add basic prediction based backtesting strategy
    Introduce simple backtesting strategy that manages positions based on
    external predictions. This allows us to decouple the prediction logic
    from the general backtesting strategy.
    
    The logic of the prediction-based strategy is as follows:
    - On each feed update, recompute the latest prediction
    - If the prediction is more than half a tick (default $0.01) above or
    below the current price then submit an order to buy or sell accordingly
    - The order size is configurable (default 100) and capped by a max
    position size (default 10000), as well as the outstanding account cash
    balance
    - On the start of the next feed update, cancel previous active orders
    that have not been filled

--- src/backtest/strategy.py
+++ src/backtest/strategy.py
@@ -30,6 +30,91 @@ from pyalgotrade.bar import Frequency
 #         y_prev = y_pred.detach()
     
 #     return valid_loss.item()
+class Predictor:
+    def predict(self, date_time_series, close_data_series):
+        pass
+
+
+class PredictorStrategy(strategy.BacktestingStrategy):
+    def __init__(self, feed, instrument, predictor, tick_size=0.01, lot_size=100, max_position=10000):
+        super(PredictorStrategy, self).__init__(feed)
+        self.instrument = instrument
+        self.last_position = None
+        self.initialPortfolio = self.getBroker().getEquity()
+        self.predictor = predictor
+        self.tick_size = tick_size
+        self.lot_size = lot_size
+        self.max_position = max_position
+
+    def onStart(self):
+        self.info(f"Starting run. Initial portfolio value: $%.f" % self.initialPortfolio)
+
+    def onFinish(self, bars):
+        broker = self.getBroker()
+        portfolio = broker.getEquity()
+        profit = portfolio - self.initialPortfolio
+        returns = profit / self.initialPortfolio
+        date_times = self.getFeed().getDataSeries().getDateTimes()
+        trading_days = len(set([date_time.date() for date_time in date_times]))
+        year_fraction = trading_days / 252.0
+        daily_returns = (returns + 1.0) ** (1.0 / trading_days) - 1.0
+        annual_returns = (returns + 1.0) ** (1.0 / year_fraction) - 1.0
+        self.info(f"Completed run. Final portfolio value: $%.2f. Final PnL: $%.2f. Daily return: %.2f%%. Annual return: %.2f%%. Current position %.1f"
+                  % (portfolio, profit, daily_returns * 100.0, annual_returns * 100.0, broker.getShares(self.instrument)))
+
+    def onEnterOk(self, position):
+        order = position.getEntryOrder()
+        execInfo = order.getExecutionInfo()
+        self.info(f"%s at $%.2f" % ("BUY" if order.isBuy() else "SELL", execInfo.getPrice()))
+
+    def onExitOk(self, position):
+        order = position.getExitOrder()
+        execInfo = order.getExecutionInfo()
+        self.info(f"%s at $%.2f" % ("BUY" if order.isBuy() else "SELL", execInfo.getPrice()))
+        self.last_position = None
+
+    def onBars(self, bars):
+        if self.last_position is not None and self.last_position.entryActive():
+            last_order = self.last_position.getEntryOrder()
+            self.debug(f"CANCEL open order to %s at $%.2f" % ("BUY" if last_order.isBuy() else "SELL", last_order.getLimitPrice()))
+            self.last_position.cancelEntry()
+            self.last_position = None
+
+        bar = bars[self.instrument]
+        close_price = bar.getClose()
+        self.debug(f"New price: $%.2f" % close_price)
+
+        feed_series = self.getFeed().getDataSeries()
+        prediction = self.predictor.predict(feed_series.getDateTimes(), feed_series.getCloseDataSeries())
+
+        broker = self.getBroker()
+        current_shares = broker.getShares(self.instrument)
+
+        buy_target_price = close_price + self.tick_size / 2.0
+        sell_target_price = close_price - self.tick_size / 2.0
+        if prediction is None:
+            return
+        elif prediction > buy_target_price:
+            cash = broker.getCash()
+            buy_capacity = cash / buy_target_price
+            max_shares_allowed = self.max_position - current_shares
+
+            quantity = max(min(self.lot_size, buy_capacity, max_shares_allowed), 0)
+            self.last_position = self.enterLongLimit(self.instrument, buy_target_price, quantity)
+            self.debug(f"Submitting order to BUY %.1f at $%.2f" % (quantity, buy_target_price))
+        elif prediction < sell_target_price:
+            max_shares_allowed = self.max_position + current_shares
+
+            quantity = min(self.lot_size, max_shares_allowed)
+            self.last_position = self.enterShortLimit(self.instrument, sell_target_price, quantity)
+            self.debug(f"Submitting order to SELL %.1f at $%.2f" % (quantity, sell_target_price))
+
+
+class DummyPredictor(Predictor):
+
+    def predict(self, date_time_series, close_data_series):
+        if len(close_data_series) > 5:
+            return close_data_series[-1] + (close_data_series[-1] - close_data_series[-2])
 
 
 class LongShortStrategy(strategy.BacktestingStrategy):
@@ -70,7 +155,8 @@ class LongShortStrategy(strategy.BacktestingStrategy):
 feed = csvfeed.GenericBarFeed(frequency=Frequency.MINUTE)
 feed.addBarsFromCSV("qqq", "qqq_data.csv")
 
-strategy = LongShortStrategy(feed, "qqq")
+# strategy = LongShortStrategy(feed, "qqq")
+strategy = PredictorStrategy(feed, "qqq", DummyPredictor())
 strategy.run()
 portfolio_value = strategy.getBroker().getEquity() + strategy.getBroker().getCash()
 print(portfolio_value)
