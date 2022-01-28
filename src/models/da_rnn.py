5a97f71792e0ea3657f3b54e72361e75ec0a9c48
rganti <ramanganti@gmail.com>
2022-01-28 14:52:15 -0500

    Made minor modifications

--- src/models/da_rnn.py
+++ src/models/da_rnn.py
@@ -1,10 +1,8 @@
 """
-DA-RNN model architecture.
-
-References:
-    [1] Yao Qin, Dongjin Song, Haifeng Chen, Wei Cheng, Guofei Jiang, Garrison W. Cottrell.
-        "A Dual-Stage Attention-Based Recurrent Neural Network for Time Series Prediction"
-        arXiv preprint arXiv:1704.02971 (2017).
+DA-RNN model architecture as described in:
+Yao Qin, Dongjin Song, Haifeng Chen, Wei Cheng, Guofei Jiang, Garrison W. Cottrell.
+"A Dual-Stage Attention-Based Recurrent Neural Network for Time Series Prediction"
+arXiv preprint arXiv:1704.02971 (2017).
 """
 
 import matplotlib.pyplot as plt
